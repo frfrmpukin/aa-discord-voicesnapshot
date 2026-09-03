19d6cc6f28cb9ecc01b5e722a53a75e26b57caf6
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-03 01:02:20 -0500

    Add URL and menu item hooks for Discord Voice Snapshot

--- aa_discord_voicesnapshot/auth_hooks.py
+++ aa_discord_voicesnapshot/auth_hooks.py
@@ -1,7 +1,10 @@
 from allianceauth import hooks
-from allianceauth.services.hooks import UrlHook
+from allianceauth.services.hooks import UrlHook, MenuItemHook
 
 
+#
+# URL HOOK
+#
 @hooks.register('url_hook')
 def register_urls():
     return UrlHook(
@@ -9,3 +12,26 @@ def register_urls():
         urlconf='aa_discord_voicesnapshot.urls',
         prefix='voicesnapshot/'
     )
+
+
+#
+# NAVIGATION MENU HOOK
+#
+class VoiceSnapshotMenu(MenuItemHook):
+    def __init__(self):
+        super().__init__(
+            "Discord Voice Snapshot",
+            "aa_discord_voicesnapshot:snapshot",
+            "fa fa-microphone"
+        )
+
+    def render(self, request):
+        if request.user.has_perm("aa_discord_voicesnapshot.take_snapshot") or \
+           request.user.has_perm("aa_discord_voicesnapshot.view_snapshot_history"):
+            return super().render(request)
+        return ""
+
+
+@hooks.register('menu_item_hook')
+def register_menu():
+    return VoiceSnapshotMenu()
