9eb230c34a41afab3d94bd48da5accb787b66012
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-03 00:58:05 -0500

    Clean up AADiscordVoiceSnapshotConfig
    Remove unused hooks registration in app config.

--- aa_discord_voicesnapshot/apps.py
+++ aa_discord_voicesnapshot/apps.py
@@ -1,10 +1,5 @@
 from django.apps import AppConfig
-from allianceauth import hooks
-from .navigation import VoiceSnapshotMenu
 
 class AADiscordVoiceSnapshotConfig(AppConfig):
     name = "aa_discord_voicesnapshot"
     verbose_name = "Discord Voice Snapshot"
-
-    def ready(self):
-        hooks.register(VoiceSnapshotMenu())
