fd1892a3421a5267822a29d57cfa912121c2953d
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-07 11:09:13 -0500

    Fix URL name in VoiceSnapshotMenu initialization

--- aa_discord_voicesnapshot/auth_hooks.py
+++ aa_discord_voicesnapshot/auth_hooks.py
@@ -20,10 +20,11 @@ def register_urls():
 class VoiceSnapshotMenu(MenuItemHook):
     def __init__(self):
         super().__init__(
-            "Discord Voice Snapshot",              # text (label)
-            "fa fa-microphone",                    # classes (icon)
-            "aa_discord_voicesnapshot:snapshot",   # url_name
+            "Discord Voice Snapshot",      # text
+            "fa fa-microphone",            # icon
+            "voicesnapshot:snapshot",      # CORRECT url_name
         )
+
 
     def render(self, request):
         if request.user.has_perm("aa_discord_voicesnapshot.take_snapshot") or \
