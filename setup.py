06a59a27a9c01b2702a2bb2a6cf6d821456beaa8
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-07 14:15:39 -0500

    Bump version to 1.0.1 and update package description
    Updated version to 1.0.1 and enhanced description with styling and UI improvements.

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="aa-discord-voicesnapshot",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     include_package_data=True,
     package_data={"aa_discord_voicesnapshot": ["templates/aa_discord_voicesnapshot/*.html"]},
     install_requires=["requests"],
-    description="Alliance Auth plugin to snapshot Discord voice channel occupants",
+    description="
+    Alliance Auth plugin to snapshot Discord voice channel occupants
+    - Styled all templates to match Alliance Auth
+    - Improved UI consistency
+    - Updated setup.py packaging
+    ",
     author="FrFrmPukin",
     url="https://github.com/frfrmpukin/aa-discord-voicesnapshot",
 )
