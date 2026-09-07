db34c298201101657cca7e2be2f9391fe43ce0e0
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-07 14:55:45 -0500

    Update setup.py for version 1.0.2 changes
    Updated version information in setup.py to reflect changes in 1.0.2.

--- setup.py
+++ setup.py
@@ -9,10 +9,8 @@ setup(
     install_requires=["requests"],
     description="""Alliance Auth plugin to snapshot Discord voice channel occupants
 
-Changes in 1.0.1:
-- Styled all templates to match Alliance Auth
-- Improved UI consistency
-- Updated setup.py packaging
+Changes in 1.0.2:
+- Fixed Errors in Template Files
 """,
     author="FrFrmPukin",
     url="https://github.com/frfrmpukin/aa-discord-voicesnapshot",
