f8b2af2ec46371396544a40fdd7b881b2eec73a7
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-07 16:13:18 -0500

    Bump version to 1.0.3 and enhance functionality
    Updated version to 1.0.3 and added new features.

--- setup.py
+++ setup.py
@@ -2,16 +2,28 @@ from setuptools import setup, find_packages
 
 setup(
     name="aa-discord-voicesnapshot",
-    version="1.0.2",
+    version="1.0.3",
     packages=find_packages(),
     include_package_data=True,
-    package_data={"aa_discord_voicesnapshot": ["templates/aa_discord_voicesnapshot/*.html"]},
-    install_requires=["requests"],
+    package_data={
+        "aa_discord_voicesnapshot": [
+            "templates/aa_discord_voicesnapshot/*.html"
+        ]
+    },
+    install_requires=[
+        "requests",
+        "websocket-client",
+    ],
     description="""Alliance Auth plugin to snapshot Discord voice channel occupants
 
-Changes in 1.0.2:
-- Fixed Errors in Template Files
+Changes in 1.0.3:
+- Added real-time Discord voice tracking via Gateway
+- Added VoiceState model
+- Added gateway listener thread
+- Updated snapshot logic to use live voice data
+- Fixed template rendering issues
 """,
     author="FrFrmPukin",
     url="https://github.com/frfrmpukin/aa-discord-voicesnapshot",
 )
+
