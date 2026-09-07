41f6dcd5d2344da3eedc7ce275908b6256a7a816
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-07 12:14:09 -0500

    Refactor AADiscordVoiceSnapshotConfig class
    Refactor app configuration and inject URLs into main URL tree.

--- aa_discord_voicesnapshot/apps.py
+++ aa_discord_voicesnapshot/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
+from django.urls import include, path
 
-class AADiscordVoiceSnapshotConfig(AppConfig):
-    name = "aa_discord_voicesnapshot"
-    verbose_name = "Discord Voice Snapshot"
 
+class AaDiscordVoiceSnapshotConfig(AppConfig):
+    name = 'aa_discord_voicesnapshot'
+
+    def ready(self):
+        # Import the global Alliance Auth URL list
+        from allianceauth.urls import urlpatterns
+
+        # Inject this plugin's URLs into AA's main URL tree
+        urlpatterns += [
+            path('voicesnapshot/', include('aa_discord_voicesnapshot.urls')),
+        ]
