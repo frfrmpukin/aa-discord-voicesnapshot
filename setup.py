1e5bdf3900bf5671d29ace9dcba3c009912f14a2
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-07 14:17:32 -0500

    Revise package description in setup.py
    Updated the description field to include version changes and improvements.

--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ setup(
     include_package_data=True,
     package_data={"aa_discord_voicesnapshot": ["templates/aa_discord_voicesnapshot/*.html"]},
     install_requires=["requests"],
-    description="
-    Alliance Auth plugin to snapshot Discord voice channel occupants
-    - Styled all templates to match Alliance Auth
-    - Improved UI consistency
-    - Updated setup.py packaging
-    ",
+    description="""Alliance Auth plugin to snapshot Discord voice channel occupants
+
+Changes in 1.0.1:
+- Styled all templates to match Alliance Auth
+- Improved UI consistency
+- Updated setup.py packaging
+""",
     author="FrFrmPukin",
     url="https://github.com/frfrmpukin/aa-discord-voicesnapshot",
 )
