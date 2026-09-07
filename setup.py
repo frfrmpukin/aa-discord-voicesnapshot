ae448edb9eadf88576e4f6800ac81f084daaff31
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-07 13:55:52 -0500

    Add package_data for HTML templates

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="1.0.0",
     packages=find_packages(),
     include_package_data=True,
+    package_data={"aa_discord_voicesnapshot": ["templates/aa_discord_voicesnapshot/*.html"]},
     install_requires=["requests"],
     description="Alliance Auth plugin to snapshot Discord voice channel occupants",
     author="FrFrmPukin",
