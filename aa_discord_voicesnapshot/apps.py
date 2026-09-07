06d66a2ffad31dd29efd9db541e85a0650a17591
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-07 15:55:27 -0500

    Initialize Discord voice gateway listener on startup
    Added a Discord voice gateway listener to the app's ready method.

--- aa_discord_voicesnapshot/apps.py
+++ aa_discord_voicesnapshot/apps.py
@@ -1,15 +1,18 @@
 from django.apps import AppConfig
 from django.urls import include, path
-
+import threading
 
 class AaDiscordVoiceSnapshotConfig(AppConfig):
     name = 'aa_discord_voicesnapshot'
 
     def ready(self):
-        # Import Alliance Auth's global URL list
+        # Register URLs
         from allianceauth.urls import urlpatterns
-
-        # Register this plugin's URLs under /voicesnapshot/
         urlpatterns += [
             path('voicesnapshot/', include('aa_discord_voicesnapshot.urls')),
         ]
+
+        # Start Discord voice gateway listener
+        from .gateway import VoiceGatewayClient
+        client = VoiceGatewayClient()
+        client.start()
