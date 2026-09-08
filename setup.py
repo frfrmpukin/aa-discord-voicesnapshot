39100a35f8668fb449e402300c4675a9732a6596
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-08 16:33:20 -0500

    Bump version to 1.0.9 and update changelog
    Updated version from 1.0.8 to 1.0.9 and modified description.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aa-discord-voicesnapshot",
-    version="1.0.8",
+    version="1.0.9",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -11,10 +11,8 @@ setup(
     ],
     description="""Alliance Auth plugin to snapshot Discord voice channel occupants
 
-Changes in 1.0.8:
-- Fixed packaging issues by removing package_data
-- MANIFEST.in now correctly includes all module files
-- Ensures AppConfig loads and gateway thread starts
+Changes in 1.0.9:
+- Simplified the initialization of the VoiceGatewayClient.
 """,
     author="FrFrmPukin",
     url="https://github.com/frfrmpukin/aa-discord-voicesnapshot",
