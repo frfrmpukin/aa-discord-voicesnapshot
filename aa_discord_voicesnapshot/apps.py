f316bc7212a1c2c281269c082dccb8a99f137e3f
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-08 16:51:13 -0500

    Refactor VoiceGatewayClient startup to post-migrate
    Connect start_gateway function to post_migrate signal to initiate VoiceGatewayClient after migrations.

--- aa_discord_voicesnapshot/apps.py
+++ aa_discord_voicesnapshot/apps.py
@@ -1,8 +1,12 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+def start_gateway(sender, **kwargs):
+    from aa_discord_voicesnapshot.gateway import VoiceGatewayClient
+    VoiceGatewayClient().start()
 
 class AaDiscordVoiceSnapshotConfig(AppConfig):
     name = 'aa_discord_voicesnapshot'
 
     def ready(self):
-        from .gateway import VoiceGatewayClient
-        VoiceGatewayClient().start()
+        post_migrate.connect(start_gateway, sender=self)
