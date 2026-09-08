8e7871ed3d29022f77f17506dbd416c2cf5fd1cd
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-08 16:21:53 -0500

    Refactor VoiceGatewayClient initialization
    Simplified the initialization of the VoiceGatewayClient.

--- aa_discord_voicesnapshot/apps.py
+++ aa_discord_voicesnapshot/apps.py
@@ -1,18 +1,8 @@
 from django.apps import AppConfig
-from django.urls import include, path
-import threading
 
 class AaDiscordVoiceSnapshotConfig(AppConfig):
     name = 'aa_discord_voicesnapshot'
 
     def ready(self):
-        # Register URLs
-        from allianceauth.urls import urlpatterns
-        urlpatterns += [
-            path('voicesnapshot/', include('aa_discord_voicesnapshot.urls')),
-        ]
-
-        # Start Discord voice gateway listener
         from .gateway import VoiceGatewayClient
-        client = VoiceGatewayClient()
-        client.start()
+        VoiceGatewayClient().start()
