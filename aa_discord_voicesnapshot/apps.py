dfe3ad4ddf7a91e5cd83c33e6554d88b61d21466
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-08 23:02:00 -0500

    Refactor app initialization to use logging
    Replaced post_migrate signal with logging and direct gateway start.

--- aa_discord_voicesnapshot/apps.py
+++ aa_discord_voicesnapshot/apps.py
@@ -1,12 +1,12 @@
+import logging
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
 
-def start_gateway(sender, **kwargs):
-    from aa_discord_voicesnapshot.gateway import VoiceGatewayClient
-    VoiceGatewayClient().start()
+logger = logging.getLogger(__name__)
 
 class AaDiscordVoiceSnapshotConfig(AppConfig):
-    name = 'aa_discord_voicesnapshot'
+    name = "aa_discord_voicesnapshot"
 
     def ready(self):
-        post_migrate.connect(start_gateway, sender=self)
+        logger.info("VoiceSnapshot: AppConfig.ready() starting gateway thread")
+        from aa_discord_voicesnapshot.gateway import VoiceGatewayClient
+        VoiceGatewayClient().start()
