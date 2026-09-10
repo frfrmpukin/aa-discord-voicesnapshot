7aa638a15cff423930b48f8846921a1a8f3bff87
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-10 07:03:05 -0500

    Fix syntax error in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aa-discord-voicesnapshot",
-    version="1.0.13 packages=find_packages(),
+    version="1.0.13" packages=find_packages(),
     include_package_data=True,
     install_requires=[
         "requests",
