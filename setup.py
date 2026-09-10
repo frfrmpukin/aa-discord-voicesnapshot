75e4bc22f7c1fd5b5dd0411af84f550f02d4df9d
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-10 07:04:49 -0500

    Fix formatting in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="aa-discord-voicesnapshot",
-    version="1.0.13" packages=find_packages(),
+    version="1.0.13",
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         "requests",
@@ -23,3 +24,4 @@ gateway: defer Django model imports and resolve token at runtime
     author="FrFrmPukin",
     url="https://github.com/frfrmpukin/aa-discord-voicesnapshot",
 )
+
